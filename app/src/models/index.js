5616d0ba746ecb644add437eadf64feec9a8feb6
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-09-04 04:31:52 +0300

    Add CraftsmanProfile Model
    - Add Mongoose CraftsmanProfile Model

--- app/src/models/index.js
+++ app/src/models/index.js
@@ -12,7 +12,11 @@ const User = require('./User')(mongoose, validator);
 //Creates and exports the ClientProfile model.
 const ClientProfile = require('./ClientProfile')(mongoose);
 
+//Creates and exports the CraftsmanProfile model.
+const CraftsmanProfile = require('./CraftsmanProfile')(mongoose);
+
 module.exports = {
     User,
     ClientProfile,
+    CraftsmanProfile,
 };
