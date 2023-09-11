d111a58288bc1c9e4928a888ff4764b67d6ec12d
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-09-11 17:36:49 +0300

    Update `app.js`
    -  Mount routes in createApp function

--- app/src/app.js
+++ app/src/app.js
@@ -1,20 +1,21 @@
 // Import dependencies
 const express = require('express');
 const middlewareLoader = require('./middlewares');
+const routes = require('./routes');
 
 /**
  * Creates and configures an Express.js application.
  *
+ * @function createApp
  * @param {Database} database - The Database object used for database connection.
  * @returns {Express} An instance of the configured Express application.
  * @throws {Error} If the database connection fails.
- *
  * @example
- * // Create and configure an Express app with a connected database.
+ * const database = new Database(config);
  * const app = createApp(database);
  */
 async function createApp(database) {
-    // connect to database
+    // Connect to database
     database
         .connect()
         .then(
@@ -25,10 +26,13 @@ async function createApp(database) {
             )
         );
 
-    // create express app
+    // Create express app
     const app = express();
 
     middlewareLoader(app).pre();
+
+    // Mount routes
+    routes(app, middlewareLoader(app).customMiddlewares);
 
     middlewareLoader(app).post();
 
